481c4bea9709be45310740434fd00eea9aa79e44
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2024-12-10 11:56:22 +0200

    Create and apply migrations, create serializers for all created models

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
-from cinema.models import Movie
+from cinema.models import (
+    Movie,
+    Actor,
+    Genre,
+    CinemaHall,
+)
 
 
 class MovieSerializer(serializers.Serializer):
@@ -22,3 +27,21 @@ class MovieSerializer(serializers.Serializer):
         instance.save()
 
         return instance
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ["name"]
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ["first_name", "last_name"]
+
+
+class CinemaHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CinemaHall
+        fields = ["name", "rows", "seats_in_row"]
